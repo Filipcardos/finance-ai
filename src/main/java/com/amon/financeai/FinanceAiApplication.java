d59d9e091269608326bd703c3f95bb0fb9100945
Filipe Oliveira Cardoso <filipe.cardoso@afya.com.br>
2026-05-25 15:14:52 -0300

    Projeto inicial Smart Finance AI com Spring Boot

---
+++ src/main/java/com/amon/financeai/FinanceAiApplication.java
@@ -0,0 +1,13 @@
+package com.amon.financeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amon/financeai/FinanceAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amon.financeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
